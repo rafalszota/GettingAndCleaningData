3736a574102cfdce265e40242a13bc19fbe956c6
rafalszota <szota.rafal@gmail.com>
2015-04-22 15:16:54 +0200

    Updated script and readme file.

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# GettingAndCleaningData
+#Script  
+
+Script follow project assignment requirements:
+
+1. Merges the training and the test sets to create one data set.
+ 
+2. Extracts only the measurements on the mean and standard deviation 
+ for each measurement. 
+ 
+3. Uses descriptive activity names to name the activities in the data set
+ 
+4. Appropriately labels the data set with descriptive variable names. 
+ 
+5. From the data set in step 4, creates a second, 
+ independent tidy data set with the average of each variable 
+ for each activity and each subject.
+
+
+#Precondition
+
+Script assumes Samsung data is unziped and availible in 
+
+    $[working.directory]/Samsung/
+
+
+
+#Steps
+##Step 1
+
+###Read all data into R
+
+- Read meta-data - features name and activities names
+- Read train dataset
+- Read test dataset
+
+
+###Work with metadata
+
+- Convert metadata into appropiate headers for x dataset
+- Set appropiate headers for actitity data set
+
+
+###Work with train data
+
+- Setup column names for x, y and subject
+- Merge y and activity so that the activity names are descriptive
+- Bind x,y and subject together
+
+
+###Work with test data
+
+- Setup column names for x, y and subject
+- Merge y and activity so that the activity names are descriptive
+- Bind x,y and subject together
+
+
+### Result 
+- Merge data-set train and test
+
+
+
+##Step 2 
+Extracts only columns for:
+
+- *activity name*, 
+- *subject*, 
+- columns with *means* or *std* within the name.
+
+
+##Step 3
+Usage of  descriptive activity names, is implied by step 1. Merge between dataframes *y* and *activity* ensures merged data set contains descriptive names.
+
+
+##Step 4
+
+The script assigns appropriate variable names, removing white-spaces, replacing special characters by dot (.) and ensures there are no several consecutive dots. 
+                               
+##Step 5		
+		
+The script aggregates measures by activity name and subject using *mean* as aggregation function. Aggregated data is stored in new clean dataset.
+			   
+#Result
+
+Script writes output data into 
+    
+	 $[working.directory]/result.txt
--- run_analysis.R
+++ run_analysis.R
@@ -12,57 +12,86 @@
 # for each activity and each subject.
 
 
+#Read all data into R
 
-xheaders <- read.table("mob/features.txt")
+## Read meta-data - features name and activities names
+xheaders <- read.table("Samsung/features.txt")
+activity <- read.table("Samsung/activity_labels.txt")
+
+## Read train dataset
+xtrain <- read.table("Samsung/train/X_train.txt")
+ytrain <- read.table("Samsung/train/y_train.txt")
+subjecttrain <- read.table("Samsung/train/subject_train.txt")
+
+## Read test dataset
+xtest <- read.table("Samsung/test/X_test.txt")
+ytest <- read.table("Samsung/test/y_test.txt")
+subjecttest <- read.table("Samsung/test/subject_test.txt")
+
+#Work with metadata
+
+##Convert metadata into appropiate headers for x dataset
 xheaders <- xheaders[, 2]
 
-activity <- read.table("mob/activity_labels.txt")
+##Set appropiate headers for actitity data set
 names(activity) <- c("id_activity", "activity.name")
 
 
-#train
-xtrain <- read.table("mob/train/X_train.txt")
+#Work with train data
+
+##Setup column names for x, y and subject
 names(xtrain) <- xheaders
-
-
-ytrain <- read.table("mob/train/y_train.txt")
 names(ytrain) <- "id_activity"
+names(subjecttrain) <- "subject"
+
+
+## Merge y and activity so that the activity names are descriptive
 activitytrain <- merge(ytrain, activity, by="id_activity")
 
 
-subjecttrain <- read.table("mob/train/subject_train.txt")
-names(subjecttrain) <- "subject"
-
+## Bind x,y and subject together
 train <- cbind(activitytrain, subjecttrain, xtrain)
 
-#test
-xtest <- read.table("mob/test/X_test.txt")
+
+# Repeat above steps for test data
+
+##Setup column names for x, y and subject
 names(xtest) <- xheaders
-
-
-ytest <- read.table("mob/test/y_test.txt")
 names(ytest) <- "id_activity"
+names(subjecttest) <- "subject"
+
+
+## Merge y and activity so that the activity names are descriptive
 activitytest <- merge(ytest, activity, by="id_activity")
 
 
-subjecttest <- read.table("mob/test/subject_test.txt")
-names(subjecttest) <- "subject"
-
+## Bind x,y and subject together
 test <- cbind(activitytest, subjecttest, xtest)
 
+
+#Result step one merged data-set train and test
 data <- rbind(test, train)
 
-#extracts only data for means
+
+
+#Step two - extracts only columns for activity name, subject, and
+#means or std within the name
 mean.std.data <- data[, c(2, 3, grep(".*(mean|std)[\\(].*", names(data)))]
 
-#sets appropiate variable names
+
+#Step three is implied by step one
+
+
+#Step four - appropriate variable names
 correct.var.names <- make.names(names(mean.std.data))
 correct.var.names <- gsub("\\.{2, }", ".", correct.var.names)
 names(mean.std.data) <- correct.var.names
-                                
-#create new clean dataset
+                               
+							   
+#Step five - create new clean dataset written into file
 clean.data <- aggregate(mean.std.data[, -c(1, 2)], by=list(mean.std.data$activity.name, mean.std.data$subject), FUN=mean, na.rm=TRUE)                                
 names(clean.data)[1] <- "activity.name"
 names(clean.data)[2] <- "subject"
 
+write.table("result.txt", row.name=FALSE) 
 
